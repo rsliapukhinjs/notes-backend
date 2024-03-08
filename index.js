a263affaf9345110cd3f6e0994fa59d203bf803d
rsliapukhinjs <rsliapukhinjs@gmail.com>
2024-03-08 21:00:31 +0300

    after exercise 3.22

--- index.js
+++ index.js
@@ -41,27 +41,24 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: "content is missing",
-    });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => response.json(savedNote));
+  note
+    .save()
+    .then((savedNote) => response.json(savedNote))
+    .catch((error) => next(error));
 });
 //   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
 //   return maxId + 1;
 // };
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end();
@@ -77,7 +74,11 @@ app.put("/api/notes/:id", (request, response, next) => {
     important: body.important,
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedNote) => response.json(updatedNote))
     .catch((error) => next(error));
 });
@@ -94,6 +95,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "Wrong format id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
